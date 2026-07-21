7cef6a0c9b46a5c5236b7e817d5b8e0edf2575f9
anjomozda <anjamilovanovic.kg@gmail.com>
2026-07-21 09:23:36 +0200

    Dodaj PWA podrsku: manifest, service worker, ikonice (instalacija na pocetni ekran)

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Bump ovu verziju kad promeniš fajlove, da se keš osveži.
+const CACHE = "pomodoro-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Cache-first: radi i offline, jer je sve statika.
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request))
+  );
+});
